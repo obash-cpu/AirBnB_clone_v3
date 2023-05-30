3b4e74ea000c2c01a907a421bf08fa92bc4d8667
Robin60 <owinorobin05@gmaaail.com>
2023-05-30 16:33:17 +0000

    fix: Fixes on views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+""" initializes view of directory  """
+
+
 from flask import Blueprint
 
 
@@ -5,9 +9,3 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.amenities import *
-from api.v1.views.places import *
-from api.v1.views.cities import *
-from api.v1.views.users import *
-from api.v1.views.places_reviews import *
